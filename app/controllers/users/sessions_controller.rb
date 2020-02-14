5b86e7f5b63ab85a0a33bd3e3d3b96e9ce957a9d
omorihirofumi <kanazawabunko827@icloud.com>
2020-02-14 18:30:04 +0900

    部分テンプレート変更

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,13 +1,4 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
-  def login
-    @aaa = abc
-    @user = User.find_by(email: params[:email], password: params[:password])
-    if @user
-      flash[:notice] = "ログインしました"
-    else
-      @error_message = "メールアドレスまたはパスワードが間違っています"
-    end
-  end
 end
