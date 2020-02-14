32a2bfbddc118ab134e9f52ad32fcb0cb3f98ea8
Nakamoto-art <bclarence747@gmail.com>
2020-02-14 20:53:30 +0900

    新規登録機能４
    ・submitボタンの編集
    ・validateの追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,23 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+#   validates :nickname, presence: true
+
+#   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+#   validates :email, format: {with: VALID_EMAIL_REGEX}, allow_blank: true
+#   validates :email, presence: true
+
+#   validates :encrypted_password, presence: true
+#   validates :encrypted_password, length: { in: 6..128 }
+
+#   validates :lastname, presence: true
+
+#   validates :firstname, presence: true
+
+#   validates :lastname_kana, presence: true
+
+#   validates :firstname_kana, presence: true
+
+#   validates :birthday, presence: true
 end
