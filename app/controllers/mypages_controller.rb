f78deb4e209af17a18da8b7f7b6322954e39a1db
shounosukeiko <toshimichi.fukushima@gmail.com>
2020-02-17 18:59:39 +0900

    マイページビュー作成
    マイページのユーザー画面、ログアウト画面、カード情報入力画面

---
+++ app/controllers/mypages_controller.rb
@@ -0,0 +1,10 @@
+class MypagesController < ApplicationController
+  def index
+  end
+  
+  def logout
+  end
+  
+  def card
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  get 'mypages/index'
+  get "mypages/logout"
+  get "mypages/card"
   devise_for :users, controllers: {
     sessions: 'users/sessions'
   }
+  
 end
