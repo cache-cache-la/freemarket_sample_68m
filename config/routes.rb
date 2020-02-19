28daadb55d02037d4fc3863af1bed0571454b735
cache-cache-la <rote.blumen.21991@gmail.com>
2020-02-19 14:35:47 +0900

    購入ページのマークアップ
    商品購入のビューのみとなります。まずアプリケーション全体のフォントを基本的に揃えるためfont.scssの作成、設定、application.scssにfontとpurchase（購入）を読み込むよう設定しました。データベース設計に相談の余地があるため、コントローラー等は編集していません。

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'mypages/index'
+  get "mypages/index"
   get "mypages/logout"
   get "mypages/card"
   devise_for :users, controllers: {
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   }
   resource :items, only: [:show]
   root "top#index"
-  get 'logout/index'
+  get "logout/index"
+  get "items/purchase"
+  # items/purchaseは画面を表示するための仮置きです
   resources :user, only:[:index, :edit, :destroy]
 end
