6695d99ce44a8188f0b40ecb75359c2cd0dffcf1
cache-cache-la <rote.blumen.21991@gmail.com>
2020-02-24 19:43:21 +0900

    commit
    エラーが生じたため、一旦コミット後ブランチを削除します。

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,21 @@ Rails.application.routes.draw do
 
   resource :user, only: :index
 
-  resource :card, only:[:index, :new, :show, :destroy]
+   #住所登録をウィザード形式で表示させる
+  devise_scope :user do
+    get 'addresses', to: 'users/registrations#new_address'
+    post 'addresses', to: 'users/registrations#create_address'
+  end
+
+  resources :card, only: [:new, :show, :destroy] do
+    collection do
+      post 'show', to: 'card#showing'
+      post 'pay', to: 'card#pay'
+    end
+  end
+
+  # 仮置き
+  get "mypages/index"
+  get "mypages/logout"
+
 end
