0c74c46658c8316192b4669daf13151892b1debd
cache-cache-la <rote.blumen.21991@gmail.com>
2020-02-24 19:53:13 +0900

    routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -10,12 +10,7 @@ Rails.application.routes.draw do
     registrations: 'users/registrations',
     sessions: 'users/sessions'
   }
-  resource :items, only: [:show]
-  root "top#index"
-  get "logout/index"
-  get "items/purchase"
-  # items/purchaseは画面を表示するための仮置きです
-  resources :user, only:[:index, :edit, :destroy]
+
   resources :card, only: [:new, :show, :destroy] do
     collection do
       post 'show', to: 'card#showing'
@@ -29,12 +24,7 @@ Rails.application.routes.draw do
     post 'addresses', to: 'users/registrations#create_address'
   end
 
-  # items/purchaseは画面を表示するための仮置きです
-  resources :user, only:[:index, :edit, :destroy]
-
   get "mypages/index"
   get "mypages/logout"
-
-  resource :card, only:[:index, :new, :show, :destroy]
 
 end
