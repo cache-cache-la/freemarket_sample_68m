ceed9e733be9ee15af27b32ef7c5a8045c8898f9
shounosukeiko <toshimichi.fukushima@gmail.com>
2020-02-27 23:13:09 +0900

    修正

--- app/assets/javascripts/card.js
+++ app/assets/javascripts/card.js
@@ -1,24 +1,14 @@
 $(function(){
   $("#token_submit").on("click", function(){
-    if( document . inputForm . number . value == "" ){
+    if( document . inputForm . number . value < 16){
         document . getElementById( 'warn1' ) . style . display = "block";
     }else{
         document . getElementById( 'warn1' ) . style . display = "none";
     }
-    if( document . inputForm . number . value < 16){
-        document . getElementById( 'warn2' ) . style . display = "block";
+    if( document . inputForm . cvcnumber . value < 3){
+      document . getElementById( 'warn2' ) . style . display = "block";
     }else{
         document . getElementById( 'warn2' ) . style . display = "none";
-    }
-    if( document . inputForm . cvcnumber . value == "" ){
-      document . getElementById( 'warn3' ) . style . display = "block";
-    }else{
-        document . getElementById( 'warn3' ) . style . display = "none";
-    }
-    if( document . inputForm . cvcnumber . value < 3){
-      document . getElementById( 'warn4' ) . style . display = "block";
-    }else{
-        document . getElementById( 'warn4' ) . style . display = "none";
     }
   });
 })
