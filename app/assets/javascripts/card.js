f9787142a49bb64972b1f44fcca46435a08d110d
shounosukeiko <toshimichi.fukushima@gmail.com>
2020-02-27 18:30:08 +0900

    コメントアウト削除

--- app/assets/javascripts/card.js
+++ app/assets/javascripts/card.js
@@ -1,8 +1,4 @@
 $(function(){
-  // $("#delete").on('click', function(){
-  //   alert("削除しました");
-  // });
-
   $("#token_submit").on("click", function(){
     if( document . inputForm . number . value == "" ){
         document . getElementById( 'warn1' ) . style . display = "block";
