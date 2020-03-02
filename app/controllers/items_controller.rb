cf5fa5ce7d68e5ede38a65f9e60cb78dc8ba44bb
cache-cache-la <rote.blumen.21991@gmail.com>
2020-03-02 15:25:05 +0900

    検証削除
    コメントアウトですがbinding.pryが残ってしまったので、削除しました。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -30,7 +30,6 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-    # binding.pry
     if @item.save!
       redirect_to root_path, alert: "出品しました"
     else
