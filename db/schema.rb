fdca51827b6e41ebffad1afd101cc662b33e46b3
shounosukeiko <toshimichi.fukushima@gmail.com>
2020-03-03 21:41:01 +0900

    お気に入り更新

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2020_03_01_031850) do
-ActiveRecord::Schema.define(version: 2020_02_25_083923) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "postal_code", null: false
@@ -34,16 +33,6 @@ ActiveRecord::Schema.define(version: 2020_02_25_083923) do
     t.bigint "user_id", null: false
     t.string "customer_id", null: false
     t.string "card_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_cards_on_user_id"
-  end
-
-  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
-    t.string "ancestry"
-    t.integer "costomer_id", null: false
-    t.integer "card_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_cards_on_user_id"
@@ -117,12 +106,9 @@ ActiveRecord::Schema.define(version: 2020_02_25_083923) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
-  add_foreign_key "addresses", "users"
-  add_foreign_key "cards", "users"
-  add_foreign_key "comments", "items"
-  add_foreign_key "comments", "users"
 
   add_foreign_key "addresses", "users"
   add_foreign_key "cards", "users"
